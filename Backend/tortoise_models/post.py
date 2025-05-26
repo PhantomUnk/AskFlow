4dbd405675f1285f9c96ceccee3a504a39d433d6
Mark <ph4ntom134@yandex.ru>
2025-05-26 17:55:35 +0300

    Fix: incorrect Data Type

--- Backend/tortoise_models/post.py
+++ Backend/tortoise_models/post.py
@@ -2,10 +2,10 @@ from tortoise import fields
 from tortoise.models import Model
 
 class Post(Model):
-    id: int = fields.BigIntField(pk=True)
-    username: str = fields.TextField(max_length=32)
-    question: str = fields.TextField()
-    answer: str = fields.TextField()
+    id: fields.Field[int] = fields.BigIntField(pk=True)
+    username: fields.Field[str] = fields.TextField(max_length=32)
+    question: fields.Field[str] = fields.TextField()
+    answer: fields.Field[str] = fields.TextField()
 
-    class Meta:
+    class Meta: # type: ignore
         table = "posts"
