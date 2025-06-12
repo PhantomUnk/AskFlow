7f2775797b0312e19a0915fbd49be7959214e8f1
Mark <ph4ntom134@yandex.ru>
2025-06-12 22:50:38 +0300

    Add: Popup from Ant Design

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -5,25 +5,32 @@ import Header from "./components/Header/Header.tsx";
 import PostPage from "./components/PostPage/PostPage.tsx";
 import QuestionWindow from "./components/QuestionWindow/QuestionWindow.tsx";
 import { ToastContainer } from "react-toastify";
+import { ConfigProvider, theme } from "antd";
 
 export default function App() {
   const [questionWindowActive, setQuestionWindowActive] =
     useState<boolean>(false);
 
-  const { currentTheme } = useTheme();
+  const { currentTheme, isDark } = useTheme();
 
   return (
-    <div className={`app ${currentTheme}`}>
-      <ToastContainer />
-      <Header setQuestionWindowActive={setQuestionWindowActive} />
+    <ConfigProvider
+      theme={{
+        algorithm: isDark ? theme.darkAlgorithm : theme.defaultAlgorithm,
+      }}
+    >
+      <div className={`app ${currentTheme}`}>
+        <ToastContainer />
+        <Header setQuestionWindowActive={setQuestionWindowActive} />
 
-      <PostPage />
+        <PostPage />
 
-      <QuestionWindow
-        active={questionWindowActive}
-        setActive={setQuestionWindowActive}
-        currentTheme={currentTheme}
-      />
-    </div>
+        <QuestionWindow
+          active={questionWindowActive}
+          setActive={setQuestionWindowActive}
+          currentTheme={currentTheme}
+        />
+      </div>
+    </ConfigProvider>
   );
 }
--- Frontend/src/components/ThemeToggler/ThemeToggler.tsx
+++ Frontend/src/components/ThemeToggler/ThemeToggler.tsx
@@ -1,11 +1,20 @@
 import "./ThemeToggler.scss";
 import { useTheme } from "../../shared";
+import { Popover } from "antd";
 
 export default function ThemeToggler() {
   const { isDark, toggleTheme } = useTheme();
+  const popoverContent = (
+    <div>
+      <strong>Change Theme</strong>
+    </div>
+  );
+
   return (
-    <button className="theme-toggle" onClick={toggleTheme}>
-      {isDark ? "☀️" : "🌙"}
-    </button>
+    <Popover content={popoverContent}>
+      <button className="theme-toggle" onClick={toggleTheme}>
+        {isDark ? "☀️" : "🌙"}
+      </button>
+    </Popover>
   );
 }
